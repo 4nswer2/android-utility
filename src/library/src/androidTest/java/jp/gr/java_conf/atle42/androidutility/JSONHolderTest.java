c28f069e46a3a1da4191bdb31562438e17ccbeba
murata_to <murata_to@micware.co.jp>
2017-03-09 13:38:29 +0900

    [update]JSONHolderにJSONArrayをJSONHolderのリストとして取得するasListメソッドを追加
    その他のas系と組み合わせることで色々取得できる感じ
    あとShortはサポートしないことにした。

--- src/library/src/androidTest/java/jp/gr/java_conf/atle42/androidutility/JSONHolderTest.java
+++ src/library/src/androidTest/java/jp/gr/java_conf/atle42/androidutility/JSONHolderTest.java
@@ -18,19 +18,58 @@ public class JSONHolderTest {
 
 	@Test
 	public void testAsRawJson() throws Exception {
-		JSONHolder holder = new JSONHolder(testRawJson);
+		JSONHolder holder = JSONHolder.create(testRawJson);
 		Log.d(TAG, "[asRawJson]" + holder.opt("json").asRawJson());
-		assertEquals("{\"string\":\"test\",\"boolean\":false,\"numerical\":65535,\"array\":[{\"string\":\"long\",\"numerical\":9223372036854775807},{\"string\":\"int\",\"numerical\":2147483647},{\"string\":\"short\",\"numerical\":32767},{\"string\":\"double\",\"numerical\":34.6820959}]}", holder.opt("json").asRawJson());
+		assertEquals(answerRawJson, holder.opt("json").asRawJson());
 	}
 
 	@Test
-	public void testAsRawJsonList() throws Exception {
-		JSONHolder holder = new JSONHolder(testRawJson);
-		assertEquals("{\"string\":\"long\",\"numerical\":9223372036854775807}", holder.opt("json", "array").asRawJsonList().get(0));
+	public void testAsList() throws Exception {
+		JSONHolder holder = JSONHolder.create(testRawJson);
+		assertEquals("{\"string\":\"long\",\"numerical\":9223372036854775807}", holder.opt("json", "json-array").asList().get(0).asRawJson());
+		assertEquals("hoge", holder.opt("json", "string-array").asList().get(0).asString());
+		assertEquals(true, holder.opt("json", "boolean-array").asList().get(0).asBoolean());
+		assertEquals(9223372036854775807L, holder.opt("json", "long-array").asList().get(0).asLong().longValue());
+		assertEquals(2147483647, holder.opt("json", "int-array").asList().get(0).asInt().intValue());
+		assertEquals(34.6786737, holder.opt("json", "double-array").asList().get(0).asDouble().doubleValue(), 0.0);
 	}
 
 	//--------------------------------------------------
 	//  test rawJson
 	//--------------------------------------------------
-	private String testRawJson = "{\"json\":{\"string\":\"test\",\"boolean\":false,\"numerical\":65535,\"array\":[{\"string\":\"long\",\"numerical\":9223372036854775807},{\"string\":\"int\",\"numerical\":2147483647},{\"string\":\"short\",\"numerical\":32767},{\"string\":\"double\",\"numerical\":34.6820959}]}}";
+	private String testRawJson =
+			"{\n" +
+			"  \"json\" : {\n" +
+			"    \"string\" : \"test\",\n" +
+			"    \"boolean\" : false,\n" +
+			"    \"numerical\" : 65535,\n" +
+			"    \"json-array\" : [\n" +
+			"      { \"string\" : \"long\", \"numerical\" : 9223372036854775807 },\n" +
+			"      { \"string\" : \"int\", \"numerical\" : 2147483647 },\n" +
+			"      { \"string\" : \"double\", \"numerical\" : 34.6820959 }\n" +
+			"    ],\n" +
+			"    \"string-array\" : [\n" +
+			"      \"hoge\",\n" +
+			"      \"fuga\"\n" +
+			"    ],\n" +
+			"    \"boolean-array\" : [\n" +
+			"      true,\n" +
+			"      false\n" +
+			"    ],\n" +
+			"    \"long-array\" : [\n" +
+			"      9223372036854775807,\n" +
+			"      9223372036854775806\n" +
+			"    ],\n" +
+			"    \"int-array\" : [\n" +
+			"      2147483647,\n" +
+			"      2147483646\n" +
+			"    ],\n" +
+			"    \"double-array\" : [\n" +
+			"      34.6786737,\n" +
+			"      135.177107\n" +
+			"    ]\n" +
+			"  }\n" +
+			"}";
+
+	private String answerRawJson     = "{\"string\":\"test\",\"boolean\":false,\"numerical\":65535,\"json-array\":[{\"string\":\"long\",\"numerical\":9223372036854775807},{\"string\":\"int\",\"numerical\":2147483647},{\"string\":\"double\",\"numerical\":34.6820959}],\"string-array\":[\"hoge\",\"fuga\"],\"boolean-array\":[true,false],\"long-array\":[9223372036854775807,9223372036854775806],\"int-array\":[2147483647,2147483646],\"double-array\":[34.6786737,135.177107]}";
 }
--- src/library/src/main/java/jp/gr/java_conf/atle42/androidutility/JSONHolder.java
+++ src/library/src/main/java/jp/gr/java_conf/atle42/androidutility/JSONHolder.java
@@ -20,25 +20,30 @@ public class JSONHolder {
 	private final Object current;
 
 	//--------------------------------------------------
+	//  create method
+	//--------------------------------------------------
+	public static JSONHolder create(String rawJson) {
+		Object object = null;
+		try {
+			object = new JSONObject(rawJson);
+		} catch (JSONException e) {
+			try { object = new JSONArray(rawJson); } catch (JSONException e1) {}
+		}
+		return new JSONHolder(object);
+	}
+
+	public static JSONHolder create(JSONObject object) {
+		return new JSONHolder(object);
+	}
+
+	public static JSONHolder create(JSONArray array) {
+		return new JSONHolder(array);
+	}
+
+	//--------------------------------------------------
 	//  constructor
 	//--------------------------------------------------
 	private JSONHolder(Object object) {
-		this.current = object;
-	}
-
-	public JSONHolder(JSONObject object) {
-		this.current = object;
-	}
-
-	public JSONHolder(JSONArray array) {
-		this.current = array;
-	}
-
-	public JSONHolder(String rawJson) {
-		JSONObject object = null;
-		try {
-			object = new JSONObject(rawJson);
-		} catch (JSONException e) {}
 		this.current = object;
 	}
 
@@ -50,7 +55,7 @@ public class JSONHolder {
 	}
 
 	public JSONHolder get(int index) throws JSONException {
-		return new JSONHolder(current instanceof JSONArray ? ((JSONArray)current).get(index) : NULL);
+		return new JSONHolder(current instanceof JSONArray  ? ((JSONArray)current).get(index) : NULL);
 	}
 
 	public JSONHolder opt(String name) {
@@ -58,7 +63,7 @@ public class JSONHolder {
 	}
 
 	public JSONHolder opt(int index) {
-		return new JSONHolder(current instanceof JSONArray ? ((JSONArray)current).opt(index) : NULL);
+		return new JSONHolder(current instanceof JSONArray  ? ((JSONArray)current).opt(index) : NULL);
 	}
 
 	public JSONHolder opt(List<Object> path) {
@@ -90,41 +95,34 @@ public class JSONHolder {
 	}
 
 	// デフォルト指定あり
-	public JSONObject   asJSONObject(JSONObject v)    { return (current instanceof JSONObject) ? ((JSONObject) current)               : v; }
-	public String       asRawJson(String v)           { return (current instanceof JSONObject) ? ((JSONObject) current).toString()    : v; }
-	public JSONArray    asJSONArray(JSONArray v)      { return (current instanceof JSONArray)  ? ((JSONArray)  current)               : v; }
-	public List<String> asRawJsonList(List<String> v) { return (current instanceof JSONArray)  ? toRawJsonList((JSONArray) current)   : v; }
-	public String       asString(String v)            { return (current instanceof String)     ? (String)      current                : v; }
-	public Boolean      asBoolean(Boolean v)          { return (current instanceof Boolean)    ? (Boolean)     current                : v; }
-	public Long         asLong(Long v)                { return (current instanceof Number)     ? ((Number)     current).longValue()   : v; }
-	public Integer      asInt(Integer v)              { return (current instanceof Number)     ? ((Number)     current).intValue()    : v; }
-	public Short        asShort(Short v)              { return (current instanceof Number)     ? ((Number)     current).shortValue()  : v; }
-	public Double       asDouble(Double v)            { return (current instanceof Number)     ? ((Number)     current).doubleValue() : v; }
+	public List<JSONHolder> asList(List<JSONHolder> v) { return (current instanceof JSONArray)  ? toList((JSONArray) current)          : v; }
+	public String           asRawJson(String v)        { return (current instanceof JSONObject) ? ((JSONObject) current).toString()    : v; }
+	public String           asString(String v)         { return (current instanceof String)     ? (String)      current                : v; }
+	public Boolean          asBoolean(Boolean v)       { return (current instanceof Boolean)    ? (Boolean)     current                : v; }
+	public Long             asLong(Long v)             { return (current instanceof Number)     ? ((Number)     current).longValue()   : v; }
+	public Integer          asInt(Integer v)           { return (current instanceof Number)     ? ((Number)     current).intValue()    : v; }
+	public Double           asDouble(Double v)         { return (current instanceof Number)     ? ((Number)     current).doubleValue() : v; }
 
 	// デフォルト指定なし
-	public JSONObject   asJSONObject()  { return asJSONObject(new JSONObject()); }
-	public String       asRawJson()     { return asRawJson(""); }
-	public JSONArray    asJSONArray()   { return asJSONArray(new JSONArray()); }
-	public List<String> asRawJsonList() { return asRawJsonList(new ArrayList<>()); }
-	public String       asString()      { return asString(""); }
-	public Boolean      asBoolean()     { return asBoolean(false); }
-	public Long         asLong()        { return asLong((long)0); }
-	public Integer      asInt()         { return asInt(0); }
-	public Short        asShort()       { return asShort((short)0); }
-	public Double       asDouble()      { return asDouble((double)0); }
+	public List<JSONHolder> asList()    { return asList(new ArrayList<>()); }
+	public String           asRawJson() { return asRawJson(""); }
+	public String           asString()  { return asString(""); }
+	public Boolean          asBoolean() { return asBoolean(false); }
+	public Long             asLong()    { return asLong((long)0); }
+	public Integer          asInt()     { return asInt(0); }
+	public Double           asDouble()  { return asDouble((double)0); }
 
 	//--------------------------------------------------
 	//  private method
 	//--------------------------------------------------
-	private List<String> toRawJsonList(JSONArray array) {
-		List<String> list = new ArrayList<>();
+	private List<JSONHolder> toList(JSONArray array) {
+		List<JSONHolder> list = new ArrayList<>();
 		if (array == null) { return list; }
 
 		for (int i = 0; i < array.length(); i++) {
-			try {
-				String rawJson = array.getJSONObject(i).toString();
-				list.add(rawJson);
-			} catch (JSONException e) {}
+			Object object = null;
+			try { object = array.get(i); } catch (JSONException e) {}
+			if (object != null) { list.add(new JSONHolder(object)); }
 		}
 
 		return list;
